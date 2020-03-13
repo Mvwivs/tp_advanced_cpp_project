c065bafcfb7c38eabd159819c565616fdc9da6f8
Mvwivs <37626840+Mvwivs@users.noreply.github.com>
2020-03-14 00:01:32 +0300

    add raii descriptor wrapper

---
+++ src/process/Pipe.hpp
@@ -0,0 +1,119 @@
+
+#pragma once
+
+#include <stdexcept>
+#include <cstring>
+
+#include <unistd.h>
+
+using namespace std::string_literals;
+
+class Descriptor {
+public:
+	virtual size_t write(const void* data, size_t len) = 0;
+	virtual size_t read(void* data, size_t len) = 0;
+	virtual void close() = 0;
+};
+
+class DoubleDescriptor : public Descriptor {
+public:
+	explicit DoubleDescriptor():
+		in(-1),
+		out(-1) {
+	}
+
+	DoubleDescriptor(const DoubleDescriptor& other) = delete;
+	DoubleDescriptor& operator=(const DoubleDescriptor& other) = delete;
+
+	DoubleDescriptor(DoubleDescriptor&& other):
+		in(other.in),
+		out(other.out) {
+		other.in = -1;
+		other.out = -1;
+	}
+
+	DoubleDescriptor& operator=(DoubleDescriptor&& other) {
+		in = std::move(other.in);
+		out = std::move(other.out);
+		other.in = -1;
+		other.out = -1;
+	}
+
+	~DoubleDescriptor() {
+		close();
+	};
+
+	explicit DoubleDescriptor(int input, int output) :
+		in(input),
+		out(output) {
+	}
+
+	void closeIn() {
+		close_d(in);
+	}
+
+	void closeOut() {
+		close_d(out);
+	}
+
+	size_t write(const void* data, size_t len) override {
+		return ::write(out, data, len);
+	}
+
+	size_t read(void* data, size_t len) override {
+		return ::read(in, data, len);
+	}
+
+	void toCinCout() {
+		if (dup2(in, STDIN_FILENO) < 0) {
+			throw std::runtime_error("Error, unable to redirect to cin: "s + std::strerror(errno));
+		}
+		close_d(in);
+		if (dup2(out, STDOUT_FILENO) < 0) {
+			throw std::runtime_error("Error, unable to redirect to cout: "s + std::strerror(errno));
+		}
+		close_d(out);
+	}
+
+	void close() override {
+		close_d(in);
+		close_d(out);
+	}
+
+private:
+	static void close_d(int& fd) {
+		if (fd != -1) {
+			::close(fd);
+		}
+		fd = -1;
+	}
+
+private:
+	int in;
+	int out;
+
+};
+
+inline std::pair<DoubleDescriptor, DoubleDescriptor> createPipe() {
+	int readpipe[2] = {-1, -1};
+	int writepipe[2] = {-1, -1};
+	if (pipe(readpipe) < 0 || pipe(writepipe) < 0) {
+		for (int fd : readpipe) {
+			if (fd != -1) {
+				::close(fd);
+			}
+		}
+		for (int fd : writepipe) {
+			if (fd != -1) {
+				::close(fd);
+			}
+		}
+		throw std::runtime_error("Error, unable to create pipes: "s + std::strerror(errno));
+	}
+	return {
+		DoubleDescriptor{readpipe[0], writepipe[1]},
+		DoubleDescriptor{writepipe[0], readpipe[1]}
+	};
+}
+
+
--- src/process/Process.hpp
+++ src/process/Process.hpp
@@ -3,10 +3,9 @@
 
 #include <stdexcept>
 #include <string>
-#include <cstring>
+#include <memory>
 
-#include <sys/types.h>
-#include <unistd.h>
+#include "Pipe.hpp"
 
 using namespace std::string_literals;
 
@@ -25,57 +24,24 @@ public:
 
 	void close();
 private:
-	int readpipe[2];
-	int writepipe[2];
-
-	int& write_fd;
-	int& read_fd;
+	DoubleDescriptor descriptor;
 };
 
-Process::Process(const std::string& path):
-	readpipe{-1,-1},
-	writepipe{-1, -1},
-	read_fd(readpipe[0]),
-	write_fd(writepipe[1]) {
-	if (pipe(readpipe) < 0 || pipe(writepipe) < 0) {
-		close();
-		throw std::runtime_error("Error, unable to create pipes: "s + std::strerror(errno));
-	}
-	int& child_write = readpipe[1];
-	int& child_read = writepipe[0];
+Process::Process(const std::string& path) {
+	auto [parent, child] = createPipe();
 
 	pid_t pid = fork();
 	if (pid < 0) {
-		close();
 		throw std::runtime_error("Error, unable to fork: "s + std::strerror(errno));
 	}
 	else if (pid == 0) { // child
-		::close(write_fd);
-		::close(read_fd);
-		read_fd = -1;
-		write_fd = -1;
-		if (dup2(child_read, STDIN_FILENO) < 0) {
-			close();
-			exit(EXIT_FAILURE);
-		}
-		::close(child_read);
-		child_read = -1;
-		if (dup2(child_write, STDOUT_FILENO) < 0) {
-			close();
-			exit(EXIT_FAILURE);
-		}
-		::close(child_write);
-		child_write = -1;
-		execl(path.c_str(), path.c_str(), (char*) NULL);
+		child.toCinCout();
+		execl(path.c_str(), path.c_str(), (char*)NULL);
 		// if execl failed
-		close();
-		exit(EXIT_FAILURE);
+		throw std::runtime_error("Error, exec wasn't called: "s + std::strerror(errno));
 	}
 	else { // parent
-		::close(child_read);
-		child_read = -1;
-		::close(child_write);
-		child_write = -1;
+		descriptor = std::move(parent);
 	}
 }
 
@@ -84,7 +50,7 @@ Process::~Process() {
 }
 
 size_t Process::write(const void* data, size_t len) {
-	ssize_t written = ::write(write_fd, data, len);
+	ssize_t written = descriptor.write(data, len);
 	if (written == -1) {
 		throw std::runtime_error("Error, unable to write: "s + std::strerror(errno));
 	}
@@ -96,7 +62,7 @@ void Process::writeExact(const void* data, size_t len){
 }
 
 size_t Process::read(void* data, size_t len) {
-	ssize_t recieved = ::read(read_fd, data, len);
+	ssize_t recieved = descriptor.read(data, len);
 	if (recieved == -1) {
 		throw std::runtime_error("Error, unable to read: "s + std::strerror(errno));
 	}
@@ -108,23 +74,13 @@ void Process::readExact(void* data, size_t len) {
 }
 
 bool Process::isReadable() const {
-	return (read_fd != -1);
+
 }
 
 void Process::closeStdin() {
-	::close(write_fd);
-	write_fd = -1;
+	descriptor.closeOut();
 }
 
 void Process::close() {
-	for (int fd : readpipe) {
-		if (fd != -1) {
-			::close(fd);
-		}
-	}
-	for (int fd : writepipe) {
-		if (fd != -1) {
-			::close(fd);
-		}
-	}
+	descriptor.close();
 }
