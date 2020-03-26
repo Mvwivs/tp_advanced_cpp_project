bf857bd126aa69514ce5958b28374c7b1e26d3fc
Mvwivs <37626840+Mvwivs@users.noreply.github.com>
2020-03-26 11:12:26 +0300

    move `exec` definition to cpp

---
+++ src/process/exec_utils.cpp
@@ -0,0 +1,23 @@
+
+#include "process/exec_utils.hpp"
+
+namespace process {
+
+int exec(const std::string& path, std::vector<std::string> args) {
+	// copy vector to allow const_cast on it's content later
+
+	std::size_t total_args_cnt = args.size() + 2;
+
+	std::vector<const char*> exec_args(total_args_cnt);
+
+	exec_args[0] = path.c_str();	// program name
+	for (std::size_t i = 0; i < args.size(); ++i) {
+		exec_args[i + 1] = args[i].c_str();	// set pointers to arguments
+	}
+	exec_args[total_args_cnt - 1] = (char*)NULL;	// end indicator
+
+	// dangerous const_cast, allow execv to modify string data
+	return execv(path.c_str(), const_cast<char* const*>(exec_args.data()));
+}
+
+}
